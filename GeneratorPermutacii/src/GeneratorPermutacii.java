485a6cd3b81569925128f70d496aeeaccfca5141
felippe97 <84409435+felippe97@users.noreply.github.com>
2022-05-16 19:05:03 +0200

    .

--- GeneratorPermutacii/src/GeneratorPermutacii.java
+++ GeneratorPermutacii/src/GeneratorPermutacii.java
@@ -9,38 +9,28 @@ public class GeneratorPermutacii {
 		this.slovo = slovo;
 	}
 
-	/**
-	 * Vracia zoznam permutacii daneho slovo.
-	 * @return zoznam permutacii
-	 */
 	public ArrayList<String> getPermutacie() {
 		ArrayList<String> vysledky = new ArrayList<String>();
-		
+
 		// prazdny retazec obsahje len jedinu permutaciu a to sam seba
-		if(slovo.length() == 0)	// if(slovo.isEmpty())
+		if (slovo.length() == 0) // if(slovo.isEmpty())
 		{
 			vysledky.add(slovo);
 			return vysledky;
 		}
-		
-		// budeme iterovat cez vsetky pismena v slove
-		for(int i = 0; i < slovo.length(); i++)
-		{
-			// budeme si vytvarat slovo, ktore je skratene, t.j. odstranime z neho i-ty znak
+
+		for (int i = 0; i < slovo.length(); i++) {
+
 			String skrateneSlovo = slovo.substring(0, i) + slovo.substring(i + 1);
-			
-			// vygenerujem vsetky permutacie slova
+
 			GeneratorPermutacii skratenyGenerator = new GeneratorPermutacii(skrateneSlovo);
 			ArrayList<String> skratenePermutacie = skratenyGenerator.getPermutacie();
-			
-			// pridame odstraneny znak na zaciatok z kazdej permutacie v ramci skrateneho slova
-			for(String s : skratenePermutacie)
-			{
+
+			for (String s : skratenePermutacie) {
 				vysledky.add(slovo.charAt(i) + s);
 			}
 		}
-		
-		// vratime vsetky permutacie
+
 		return vysledky;
 	}
 }
